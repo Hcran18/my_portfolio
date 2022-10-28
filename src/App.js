ffe43ca75b95842bc1238dea8d1d8a1e9a73de61
Hunter Crandall <hunter.crandall18@gmail.com>
2022-10-28 15:26:47 +0000

    Finished Project

--- src/App.js
+++ src/App.js
@@ -14,16 +14,16 @@ import NoPage from "./pages/NoPage";
 function App() {
   return (
     <>
-    <BrowserRouter>
+    <BrowserRouter basename="/my_portfolio/build/">
       <Routes>
         <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
+          <Route index path='/' element={<Home />} />
           <Route path="About" element={<About />} />
           <Route path="Education" element={<Education />} />
           <Route path="Experience" element={<Experience />} />
           <Route path="Projects" element={<Projects />} />
-          <Route path="*" element={<NoPage />} />
           <Route path="/" element={<Footer />} />
+          <Route path="*" element={<NoPage />} />
         </Route>
       </Routes>
     </BrowserRouter>
